0b3fef8f17fa487bbf41a277abc2ff7d380c163e
hamdani415 <neswakun27@gmail.com>
2026-04-05 14:09:33 +0700

    add /start

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ bot.on("message", async (msg) => {
     if(chat === "/start") {
         bot.sendMessage(chatId , 
             "halo selamat datang di hamdani_bot_ai \n berikut adalah daftar menu yang bisa dilakukan bot ini \n1. /start untuk memulai bot \n2. ketik apa saja untuk menanyakan sesuatu dan ai akan menjawab \n3. personalisasi ai, jdi kamu bisa nyuruh ai untuk agar bersikap seperti apa.\n  contoh : kamu adalah asisten ku panggil aku (nama kamu).... \n3. /reset untuk meriset personalisai ai \n\n note: untuk menghemat limit dri api tolong sebelumnya ketik ke ai jangan jawab terlalu panjang jika tidak perlu, jawab maximal 10 kata jika memang itu tidak diperlukan, jika memang perlu boleh jawab lebih. \nTerimakasih...😊 ")
-    }
+            return
+        }
 
     if (!chats[chatId]) {
         chats[chatId] = []
